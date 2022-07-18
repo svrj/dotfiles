f79a6c44aadcf3599a5510e67a1c4ac4f157bf89
Jeremy Savor <jeremy@fotokite.com>
2022-07-18 11:34:35 +0200

    Update tailscale installation
    Update installation for Ubuntu 22.04

--- setup.sh
+++ setup.sh
@@ -47,8 +47,8 @@ i3wm() {
 # Tailscale
 # https://tailscale.com/download
 tailscale() {
-    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add -
-    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+    curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
     sudo apt update && sudo apt install tailscale
     ## Authenticate
     # sudo tailscale up
