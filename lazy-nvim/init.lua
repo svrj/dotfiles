f869e1123e8ab4e1154fa9d3c674d714bc4031af
Jeremy Savor <jeremy@fotokite.com>
2024-09-30 15:10:41 +0200

    Update specs
    Remove flashy which is redundant now (nvim already does the flashy
    highlight thing)
    
    Update tree-sitter setup

--- lazy-nvim/init.lua
+++ lazy-nvim/init.lua
@@ -41,6 +41,100 @@ require("lazy").setup({
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		{
 			"nvim-treesitter/nvim-treesitter",
+			version = false, -- last release is way too old and doesn't work on Windows
+			build = ":TSUpdate",
+			event = { "LazyFile", "VeryLazy" },
+			lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+			init = function(plugin)
+				-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+				-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+				-- no longer trigger the **nvim-treesitter** module to be loaded in time.
+				-- Luckily, the only things that those plugins need are the custom queries, which we make available
+				-- during startup.
+				require("lazy.core.loader").add_to_rtp(plugin)
+				require("nvim-treesitter.query_predicates")
+			end,
+			cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+			keys = {
+				{ "<c-space>", desc = "Increment Selection" },
+				{ "<bs>", desc = "Decrement Selection", mode = "x" },
+			},
+			opts_extend = { "ensure_installed" },
+			opts = {
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"c",
+					"diff",
+					"html",
+					"javascript",
+					"jsdoc",
+					"json",
+					"jsonc",
+					"lua",
+					"luadoc",
+					"luap",
+					"markdown",
+					"markdown_inline",
+					"printf",
+					"python",
+					"query",
+					"regex",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+				textobjects = {
+					move = {
+						enable = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+							["]a"] = "@parameter.inner",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+							["]C"] = "@class.outer",
+							["]A"] = "@parameter.inner",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+							["[a"] = "@parameter.inner",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+							["[C"] = "@class.outer",
+							["[A"] = "@parameter.inner",
+						},
+					},
+				},
+			},
+			---@param opts TSConfig
+			config = function(_, opts)
+				if type(opts.ensure_installed) == "table" then
+					opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+				end
+				require("nvim-treesitter.configs").setup(opts)
+			end,
+		},
+		--[[
+		{
+			"nvim-treesitter/nvim-treesitter",
 			build = ":TSUpdate",
 			config = function()
 				local configs = require("nvim-treesitter.configs")
@@ -52,9 +146,7 @@ require("lazy").setup({
 				})
 			end,
 		},
-		-- Flash yanked area
-		{ "kana/vim-operator-user" },
-		{ "haya14busa/vim-operator-flashy" },
+		]]
 
 		-- better JSON for Vim
 		{ "elzr/vim-json" },
